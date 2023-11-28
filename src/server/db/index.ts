6247feea8985d7fb7f89f2711163702d98c2856c
Dario Puddinu <dario.puddinu@abinsula.com>
2023-11-29 00:14:54 +0100

    db schema rework

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,8 +1,8 @@
 import { Client } from "@planetscale/database";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 
-import { env } from "~/env.mjs";
-import * as schema from "./schema";
+import { env } from "~/env.js";
+import * as schema from "./schema.js";
 
 export const db = drizzle(
   new Client({
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -19,9 +19,8 @@ export const mysqlTable = mysqlTableCreator((name) => `t3trainer_${name}`);
 export const exerciseType = mysqlEnum('exercise_type', ['sets x reps', 'stripping', 'drop set', 'giant set'])
 export const userRole = mysqlEnum('user_role', ['trainer', 'customer'])
 
-
 export const users = mysqlTable("users", {
-  id: varchar("id", { length: 255 }).notNull().primaryKey(),
+  id: bigint("id", { mode: "number" }).notNull().primaryKey(),
   name: varchar("name", { length: 255 }),
   email: varchar("email", { length: 255 }).notNull(),
   image: varchar("image", { length: 255 }),
@@ -29,51 +28,54 @@ export const users = mysqlTable("users", {
   role: userRole.notNull().default('customer')
 });
 
-export const workout = mysqlTable('workout', {
+export const workouts = mysqlTable('workout', {
   id: bigint("id", { mode: "number" }).notNull().primaryKey().autoincrement(),
   name: varchar("name", { length: 256 }),
   trainerId: int("trainer_id"),
   createdAt: date('creation_date'),
   expireAt: date('expire_date'),
+  userId: bigint("userId", { mode: "number" }).references(() => users.id)
 })
 
-export const exercise = mysqlTable('exercise', {
+export const exercises = mysqlTable('exercise', {
   id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
   name: varchar("name", { length: 256 }),
-  type: exerciseType,
+  workoutId: bigint("workoutId", { mode: "number" }).references(() => workouts.id)
+});
+
+export const bodyParts = mysqlTable('bodyparts', {
+  id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
+  name: varchar("name", { length: 256 }),
+  exerciseId: bigint("exerciseId", { mode: "number" }).references(() => exercises.id)
 });
 
 export const usersRelations = relations(users, ({ many }) => ({
-  workouts: many(workout)
+  workouts: many(workouts)
 }));
 
-export const workoutRelations = relations(workout, ({ many, one }) => ({
-  exercises: many(exercise),
+export const workoutRelations = relations(workouts, ({ many, one }) => ({
+  exercises: many(exercises),
   user: one(users, {
-    fields: [workout.id],
+    fields: [workouts.userId],
     references: [users.id]
   }),
   trainer: one(users, {
-    fields: [workout.trainerId],
+    fields: [workouts.trainerId],
     references: [users.id]
   }),
 }));
 
-export const exerciseRelations = relations(exercise, ({ one }) => ({
-  workout: one(workout, {
-    fields: [exercise.id],
-    references: [workout.id]
-  })
+export const exerciseRelations = relations(exercises, ({ one, many }) => ({
+  workout: one(workouts, {
+    fields: [exercises.workoutId],
+    references: [workouts.id]
+  }),
+  bodyParts: many(bodyParts),
 }));
 
-// export const usersToWorkoutRelations = relations(usersToWorkouts, ({ one }) => ({
-//   workout: one(workout, {
-//     fields: [usersToWorkouts.workoutId],
-//     references: [workout.id],
-//   }),
-//   user: one(user, {
-//     fields: [usersToWorkouts.userId],
-//     references: [user.id],
-//   }),
-// }));
-
+export const bodyPartsRelations = relations(bodyParts, ({ one }) => ({
+  exercise: one(exercises, {
+    fields: [bodyParts.exerciseId],
+    references: [exercises.id]
+  }),
+}));
