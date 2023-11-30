c85cc41feee6200519a20e69f180e4361259ba12
Dario Puddinu <dario.puddinu@abinsula.com>
2023-11-30 20:00:19 +0100

    created overview menu component

--- src/components/primitives/ui/menubar.tsx
+++ src/components/primitives/ui/menubar.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import * as MenubarPrimitive from "@radix-ui/react-menubar"
 import { Check, ChevronRight, Circle } from "lucide-react"
 import { type ComponentPropsWithoutRef, type ElementRef, type HTMLAttributes, forwardRef } from "react"
