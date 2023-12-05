3dc3b289485449a7fbb534b702a3460dc1f709d6
Dario Puddinu <dario.puddinu@abinsula.com>
2023-12-05 17:35:13 +0100

    feat: redirect to dashboard if user is logged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+import { redirect } from "next/navigation";
 import { LoginForm } from "~/components/forms/login";
+import { getServerAuthSession } from "~/server/auth";
 
 export default async function HomePage() {
+
+  const session = await getServerAuthSession();
+  if (session?.user) {
+    redirect('/dashboard');
+  }
+  
   return (
     <div className="w-full m-auto">
       <LoginForm />
